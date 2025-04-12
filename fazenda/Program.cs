57e2dd046ca952b7bd24e33d655835c8ca7d9daa
Felipe <felipe.araujo@a2wplataforma.com.br>
2025-04-12 00:07:50 -0300

    feat: paisagem completa

--- fazenda/Program.cs
+++ fazenda/Program.cs
@@ -1,27 +1,107 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Tao.OpenGl;
 using Tao.FreeGlut;
+
 namespace ProjetoPonto
 {
     class Program
     {
+        static float posSolX = 0; 
+        static bool ehDia = true;
+
         static void inicializa()
         {
-            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
             Gl.glLoadIdentity();
             Glu.gluOrtho2D(0, 100, 0, 100);
             Gl.glClearColor(1.0f, 1.0f, 1.0f, 0.0f);
-
         }
+
+        static void desenharSol()
+        {
+            Gl.glColor3f(1.0f, 1.0f, 0.0f);
+            Gl.glBegin(Gl.GL_TRIANGLE_FAN);
+            Gl.glVertex2f(posSolX, 90); 
+            int numLados = 100;
+            float raio = 5.0f;
+            for (int i = 0; i <= numLados; i++)
+            {
+                double ang = i * 2.0 * Math.PI / numLados;
+                float x = (float)(posSolX + raio * Math.Cos(ang));
+                float y = (float)(90 + raio * Math.Sin(ang));
+                Gl.glVertex2f(x, y);
+            }
+            Gl.glEnd();
+        }
+
+        static void desenharLua()
+        {
+            Gl.glColor3f(0.9f, 0.9f, 1.0f);
+            Gl.glBegin(Gl.GL_TRIANGLE_FAN);
+            Gl.glVertex2f(posSolX, 90);
+            int numLados = 100;
+            float raio = 4.0f;
+            for (int i = 0; i <= numLados; i++)
+            {
+                double ang = i * 2.0 * Math.PI / numLados;
+                float x = (float)(posSolX + raio * Math.Cos(ang));
+                float y = (float)(90 + raio * Math.Sin(ang));
+                Gl.glVertex2f(x, y);
+            }
+            Gl.glEnd();
+
+            // CIRCULO PARA CORTAR A LUA
+            Gl.glColor3f(0.0f, 0.0f, 0.1f); 
+            Gl.glBegin(Gl.GL_TRIANGLE_FAN);
+            Gl.glVertex2f(posSolX + 1.5f, 90);
+            float raioSombra = 4.0f;
+            for (int i = 0; i <= numLados; i++)
+            {
+                double ang = i * 2.0 * Math.PI / numLados;
+                float x = (float)(posSolX + 1.5f + raioSombra * Math.Cos(ang));
+                float y = (float)(90 + raioSombra * Math.Sin(ang));
+                Gl.glVertex2f(x, y);
+            }
+            Gl.glEnd();
+        }
+
+        static void desenharCasa()
+        {
+            Gl.glColor3f(0.6f, 0.3f, 0.0f);
+            Gl.glBegin(Gl.GL_QUADS);
+            Gl.glVertex2f(30, 40);
+            Gl.glVertex2f(50, 40);
+            Gl.glVertex2f(50, 60);
+            Gl.glVertex2f(30, 60);
+            Gl.glEnd();
+
+            Gl.glColor3f(0.8f, 0.0f, 0.0f); 
+            Gl.glBegin(Gl.GL_TRIANGLES);
+            Gl.glVertex2f(30, 60);
+            Gl.glVertex2f(50, 60);
+            Gl.glVertex2f(40, 70);
+            Gl.glEnd();
+
+            Gl.glColor3f(0.3f, 0.2f, 0.0f); 
+            Gl.glBegin(Gl.GL_QUADS);
+            Gl.glVertex2f(38, 40);
+            Gl.glVertex2f(42, 40);
+            Gl.glVertex2f(42, 50);
+            Gl.glVertex2f(38, 50);
+            Gl.glEnd();
+        }
+
         static void Paisagem()
         {
+            // Fundo do céu: muda dependendo se é dia ou noite
+            if (ehDia)
+                Gl.glClearColor(0.4f, 0.7f, 1.0f, 1.0f); 
+            else
+                Gl.glClearColor(0.0f, 0.0f, 0.1f, 1.0f); 
+
             Gl.glClear(Gl.GL_COLOR_BUFFER_BIT);
 
-            //Grama
+            // Grama
             Gl.glColor3f(0.0f, 0.7f, 0.0f);
             Gl.glBegin(Gl.GL_QUADS);
             Gl.glVertex2i(0, 40);
@@ -30,27 +110,74 @@ namespace ProjetoPonto
             Gl.glVertex2i(0, 0);
             Gl.glEnd();
 
-            //Céu
-            Gl.glColor3f(0.2f, 0.6f, 1.0f);
-            Gl.glBegin(Gl.GL_QUADS);
-            Gl.glVertex2i(0, 40);
-            Gl.glVertex2i(100, 40);
-            Gl.glColor3f(0.4f, 0.7f, 1.0f);
-            Gl.glVertex2i(100, 100);
-            Gl.glVertex2i(0, 100);
-            Gl.glEnd();
+            // Casa
+            desenharCasa();
 
+            if (ehDia)
+            {
+                desenharSol();
+            } 
+            else
+            {
+                desenharLua();
+            }
 
+            Gl.glFlush();
         }
+
+        static void AtualizarCena(int valor)
+        {
+            posSolX += 0.5f;
+
+            if (posSolX > 100)
+            {
+                posSolX = 0;
+                ehDia = !ehDia;
+            }
+
+            Glut.glutPostRedisplay();
+            Glut.glutTimerFunc(50, AtualizarCena, 0); 
+        }
+
+        static void TeclaEspecial(int tecla, int x, int y)
+        {
+            // Direita
+            if (tecla == Glut.GLUT_KEY_RIGHT)
+            {
+                posSolX += 2f;
+                if (posSolX > 100)
+                {
+                    posSolX = 0;
+                    ehDia = !ehDia;
+                }
+            }
+
+            // Esquerda
+            if (tecla == Glut.GLUT_KEY_LEFT)
+            {
+                posSolX -= 2f;
+                if (posSolX < 0)
+                {
+                    posSolX = 100;
+                    ehDia = !ehDia;
+                }
+            }
+
+            Glut.glutPostRedisplay();
+        }
+
         static void Main()
         {
             Glut.glutInit();
             Glut.glutInitDisplayMode(Glut.GLUT_SINGLE | Glut.GLUT_RGB);
             Glut.glutInitWindowSize(600, 400);
-            Glut.glutCreateWindow("Fazenda");
+            Glut.glutInitWindowPosition(100, 100);
+            Glut.glutCreateWindow("Paisagem de Fazenda");
 
             inicializa();
             Glut.glutDisplayFunc(Paisagem);
+            Glut.glutTimerFunc(0, AtualizarCena, 0);
+            Glut.glutSpecialFunc(TeclaEspecial);
             Glut.glutMainLoop();
         }
     }
